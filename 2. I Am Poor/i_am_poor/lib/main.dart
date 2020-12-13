ffad35d89ecc54e1c4d36950094ac5f533fe16c7
Devavrat Singh Bisht <devavratsinghbisht@gmail.com>
2020-12-13 21:38:44 +0530

    Added I Am Poor app and placed I Am Rich app in a floder.

---
+++ 2. I Am Poor/i_am_poor/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Poor'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coal.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
